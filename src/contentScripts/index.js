645ea60ba7dc50424556da5e3efe4e3f0db003c5
Ekko <pig211@gmail.com>
2020-05-02 23:15:31 +0800

    Scrap infos without best-off.

--- src/contentScripts/index.js
+++ src/contentScripts/index.js
@@ -1,3 +1,90 @@
 import './index.styl'
 
-console.log('Content script working...')
+
+class ClipboardWriter {
+	static createCopyableElement(text) {
+		let div = document.createElement('div');
+		div.contentEditable = true;
+		div.innerHTML = text;
+		div.unselectable = "off";
+		return div;
+	}
+
+	static copyTextToClipboard(text) {
+		let div = this.createCopyableElement(text);
+		document.body.appendChild(div);
+		div.focus();
+
+		document.execCommand('SelectAll');
+		document.execCommand("Copy", false, null);
+
+		document.body.removeChild(div);
+	}
+}
+
+
+class Scraper {
+	constructor() {
+		this.results = [];
+	}
+
+	scrap() {
+		this.results = [];
+
+		this.results.push(this._scrapTitleAndUrl());
+		this.results.push(this._scrapOriginPrice());
+		this.results.push(this._scrapBestOff());
+		this.results.push(this._scrapPublicDate());
+		this.results.push(this._scrapScore());
+		this.results.push(this._scrapCurrentDate());
+
+		return this.results;
+	}
+
+	_scrapTitleAndUrl() {
+		let title = document.querySelector('.apphub_AppName')?.innerText;
+
+		let url = document.querySelector('.blockbg>a:last-child')?.baseURI;
+		url = url.replace(/(.*?app\/\d+\/).*/, '$1');
+
+		return `=HYPERLINK("${url}","${title}")`;
+	}
+
+	_scrapOriginPrice() {
+		let price = document.querySelector('.game_purchase_action .discount_original_price,.game_purchase_price')?.innerText;
+
+		var pattern = /.*?(\d+).*/;
+		return price.replace(pattern, '$1');
+	}
+
+	_scrapBestOff() {
+		return '-';  // TODO
+	}
+
+	_scrapPublicDate() {
+		let date = document.querySelector('.release_date .date')?.innerText;
+
+		var pattern = /(\d{4}).*?(\d{1,2}).*?(\d{1,2}).*/;
+		return date.replace(pattern, '$1/$2/$3');
+	}
+
+	_scrapScore() {
+		// There are 30-days(maybe) and all-days, we want the second one
+		let scores = document.querySelectorAll('.nonresponsive_hidden.responsive_reviewdesc');
+		let score = scores[scores.length - 1]?.innerText;
+
+		var pattern = /.*?(\d+)%.*/;
+		return score.replace(pattern, '$1');
+	}
+
+	_scrapCurrentDate() {
+		return new Date().toLocaleDateString();
+	}
+}
+
+
+console.log('Start to scrap steam info ...');
+let infos =  new Scraper().scrap();
+
+console.log('Scraped info:', infos);
+ClipboardWriter.copyTextToClipboard(infos.join('	'));
